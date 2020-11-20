5c31590a1c2768e7c74c972a97ab42facc426fda
puem2639 <puem2639@gmail.com>
2020-11-20 19:14:01 +0700

    edit create

--- components/main/home.js
+++ components/main/home.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import Scsshome from './home.module.scss'
 function home() {
     return (
         <div>
-            <img src="/img/main/melissa-walker-horn-1C6HQFP7U24-unsplash.jpg" style={{width:"1520px",height:"720px"}}></img>
+                <img src="/img/main/melissa-walker-horn-1C6HQFP7U24-unsplash.jpg" className={Scsshome.homeimg1}/*style={{height:"500px",width:"500px"}}*/></img>
             <div style={{fontFamily : "Sriracha, cursive",color:"#000000"}}>
-                <p>Are you ready to</p>
-                <p>Delicious and good cakes</p>
+                <p style={{fontSize:"900%",marginLeft:"2%"}}>Are you ready to</p>
+                <p style={{fontSize:"750%",marginLeft:"2%"}}>Delicious and good cakes</p>
             </div>
         </div>
 
--- components/navbar.js
+++ components/navbar.js
@@ -6,17 +6,17 @@ function navbar() {
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" style={{background:"#FFE4E1"}}>
-  <Navbar.Brand style={{fontFamily : "Sriracha, cursive",color:"#707070"}}>CAKEBEAR</Navbar.Brand>
+  <Navbar.Brand ><img src="/img/logo.png" style={{width:"100px",height:"100px",marginLeft:"5px"}}></img></Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link style={{fontFamily : "Sriracha, cursive",color:"#707070"}}>HOME</Nav.Link>
-      <NavDropdown style={{fontFamily : "Sriracha, cursive",color:"#707070"}}  title="MENU" id="collasible-nav-dropdown">
-        <NavDropdown.Item style={{fontFamily : "Sriracha, cursive",color:"#707070"}}>Cake</NavDropdown.Item>
-        <NavDropdown.Item style={{fontFamily : "Sriracha, cursive",color:"#707070"}}>CupCake</NavDropdown.Item>
-        <NavDropdown.Item style={{fontFamily : "Sriracha, cursive",color:"#707070"}}>CheeseCake</NavDropdown.Item>
+      <Nav.Link style={{fontFamily : "Sriracha, cursive",color:"#707070",fontSize:"200%",marginLeft:"10px"}}>HOME</Nav.Link>
+      <NavDropdown style={{fontFamily : "Sriracha, cursive",color:"#707070",fontSize:"200%",marginLeft:"10px"}}  title="MENU" id="collasible-nav-dropdown">
+        <NavDropdown.Item style={{fontFamily : "Sriracha, cursive",color:"#707070",fontSize:"200%"}}>Cake</NavDropdown.Item>
+        <NavDropdown.Item style={{fontFamily : "Sriracha, cursive",color:"#707070",fontSize:"200%"}}>CupCake</NavDropdown.Item>
+        <NavDropdown.Item style={{fontFamily : "Sriracha, cursive",color:"#707070",fontSize:"200%"}}>CheeseCake</NavDropdown.Item>
       </NavDropdown>
-      <Nav.Link style={{fontFamily : "Sriracha, cursive",color:"#707070"}}>CONTACT</Nav.Link>
+      <Nav.Link style={{fontFamily : "Sriracha, cursive",color:"#707070",fontSize:"200%",marginLeft:"10px"}}>CONTACT</Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
